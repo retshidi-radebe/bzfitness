ada32cd33e29af940227b92d4fd9f4b45e6dd854
retshidi-radebe <retshidi10@gmail.com>
2026-02-24 00:14:14 +0200

    Fix: use correct adapter-libsql version and proper ES imports

--- src/lib/fresh-db.ts
+++ src/lib/fresh-db.ts
@@ -1,13 +1,10 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 export function getFreshDb() {
   if (process.env.TURSO_DATABASE_URL) {
     // Production: Turso cloud database
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { createClient } = require('@libsql/client')
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { PrismaLibSQL } = require('@prisma/adapter-libsql')
-
     const libsql = createClient({
       url: process.env.TURSO_DATABASE_URL,
       authToken: process.env.TURSO_AUTH_TOKEN,
